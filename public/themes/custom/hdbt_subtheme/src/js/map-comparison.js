a1f2a13cbc1a5ed2a8ca53eaf1566100f71b566a
samijuv <sami.juvonen@citrus.fi>
2021-10-14 06:27:32 +0300

    HIS-166 Map comparison development

--- public/themes/custom/hdbt_subtheme/src/js/map-comparison.js
+++ public/themes/custom/hdbt_subtheme/src/js/map-comparison.js
@@ -1,49 +1,81 @@
 // eslint-disable-next-line no-unused-vars
 (($, Drupal, drupalSettings) => {
+  let mainMap;
+  let comparisonMap;
+
   Drupal.behaviors.mapComparison = {
     attach: function(context, settings) {
       let self = this;
 
-      $(document).on('leaflet.map', function(e, settings, mainMap, mapid) {
-        self.bindCompareButton(mainMap);
+      $(document).on('leaflet.map', function(e, settings, lMap, mapid) {
+        mainMap = lMap;
+        self.bindCompareButton();
       });
     },
 
-    bindCompareButton: function(mainMap) {
+    bindCompareButton: function() {
       let self = this;
+      let $viewContainer = $('.views--combined-map');
       let $button = $('.map-controls__control #map-comparison-btn');
 
       $button.on('click', function(e) {
-        $('#comparison-map-container').fadeIn(150);
-        
-        let mainMapOptions = mainMap.options;
-
-        let comparisonMap = L.map('comparison-map-container', {
-          center: [mainMapOptions.center.lat, mainMapOptions.center.lon],
-          zoom: mainMapOptions.zoom,
-          zoomControl: false
-        });
-
-        mainMap.eachLayer(layer => {
-          if (layer.hasOwnProperty('_tiles')) {
-            L.tileLayer(layer._url, {
-              attribution: layer.options.attribution
-            }).addTo(comparisonMap);
-          }
-        });
-
-        comparisonMap.on('layeradd', function(e) {
-          console.log(comparisonMap);
-        });
-
-        self.bindMapEventHandlers(mainMap, comparisonMap);
+        if (!$viewContainer.is('.comparison-enabled')) {
+          self.enableMapComparison();
+        } else {
+          self.disableMapComparison();
+        }
       });
     },
+    
+    enableMapComparison: function() {
+      let self = this;
 
-    bindMapEventHandlers: function(mainMap, comparisonMap) {
+      let $viewContainer = $('.views--combined-map');
+      $viewContainer.addClass('comparison-enabled');
+
+      $('#comparison-map-container').fadeIn(150);
+
+      // Init comparison map
+      comparisonMap = L.map('comparison-map-container', {
+        center: mainMap.getCenter(),
+        zoom: mainMap.getZoom(),
+        zoomControl: false
+      });
+
+      mainMap.eachLayer(layer => {
+        if (layer.hasOwnProperty('_tiles')) {
+          L.tileLayer(layer._url, {
+            attribution: layer.options.attribution
+          }).addTo(comparisonMap);
+        }
+      });
+
+      comparisonMap.on('layeradd', function(e) {
+        console.log(comparisonMap);
+      });
+
+      self.bindMapEventHandlers();
+    },
+
+    disableMapComparison: function() {
+      let self = this;
+      let $viewContainer = $('.views--combined-map');
+
+      $viewContainer.removeClass('comparison-enabled');
+      $('#comparison-map-container').fadeOut(150);
+
+      comparisonMap.remove();
+      self.removeMapEventHandlers();
+    },
+
+    bindMapEventHandlers: function() {
       mainMap.on('move', function(ev) {
         comparisonMap.setView(mainMap.getCenter(), mainMap.getZoom());
       });
+    },
+
+    removeMapEventHandlers: function() {
+      mainMap.off('move');
     }
   };
   // eslint-disable-next-line no-undef
