e104d0ee42dba6b8a49448353a62aedddbfd16bf
veikkoa <veikko.aitosalo@citrus.fi>
2021-10-15 12:21:08 +0300

    HIS-219: HIS-219: Finna import 3.0

--- public/modules/custom/helhist_admin_forms/js/finna_import.js
+++ public/modules/custom/helhist_admin_forms/js/finna_import.js
@@ -13,51 +13,63 @@
         $.getJSON('https://api.finna.fi/api/v1/record?id=' + $finna_id + '&prettyPrint=false&lng=fi', function(data) {
 
           // Field: Formats
-          var format_data = {
-            id: '$ID:' + data['records'][0]['formats'][0]['translated'],
-            text: data['records'][0]['formats'][0]['translated']
-          };
-          // This URL should probably be resolved with regxp from #edit-field-formats...
-          $.getJSON('/select2_autocomplete/taxonomy_term/default:taxonomy_term/63DcPSsYfssc_2rLO90Cmlk-c4DwPmfe_Aj4iFdxcpM?term=' + format_data.text + '&_type=query&q=' + format_data.text, function(data) {
-            if (data['results'][0]['text'] === format_data.text) {
-              // Match found, use existing term ID
-              format_data.id = data['results'][0]['id'];
-            }
-            // Append option and select it
-            var option = new Option(format_data.text, format_data.id, true, true);
-            $('#edit-field-formats').append(option).trigger('change');
+          var formats_data = data['records'][0]['formats'];
+
+          $(formats_data).each((index, element) => {
+            var format_data = {
+              id: '$ID:' + element['translated'],
+              text: element['translated']
+            };
+            // This URL should probably be resolved with regxp from #edit-field-formats...
+            $.getJSON('/select2_autocomplete/taxonomy_term/default:taxonomy_term/63DcPSsYfssc_2rLO90Cmlk-c4DwPmfe_Aj4iFdxcpM?term=' + format_data.text + '&_type=query&q=' + format_data.text, function(data) {
+              if (data['results'].length > 0 && data['results'][0]['text'] === format_data.text) {
+                // Match found, use existing term ID for option
+                format_data.id = data['results'][0]['id'];
+              }
+              // Append option and select it
+              var option = new Option(format_data.text, format_data.id, true, true);
+              $('#edit-field-formats').append(option).trigger('change');
+            });
           });
 
           // Field: Authors
-          var author_data = {
-            id: '$ID:' + data['records'][0]['nonPresenterAuthors'][0]['name'],
-            text: data['records'][0]['nonPresenterAuthors'][0]['name']
-          };
-          // This URL should probably be resolved with regxp from #edit-field-authors...
-          $.getJSON('/select2_autocomplete/taxonomy_term/default:taxonomy_term/nL39PHzAu3uXzCPe2BBSlw4VVVuZsLVS6RtbkaMLdS8?term=' + author_data.text + '&_type=query&q=' + author_data.text, function(data) {
-            if (data['results'][0]['text'] === author_data.text) {
-              // Match found, use existing term ID
-              author_data.id = data['results'][0]['id'];
-            }
-            // Append option and select it
-            var option = new Option(author_data.text, author_data.id, true, true);
-            $('#edit-field-authors').append(option).trigger('change');
+          var authors_data = data['records'][0]['nonPresenterAuthors'];
+
+          $(authors_data).each((index, element) => {
+            var author_data = {
+              id: '$ID:' + element['name'],
+              text: element['name']
+            };
+            // This URL should probably be resolved with regxp from #edit-field-authors...
+            $.getJSON('/select2_autocomplete/taxonomy_term/default:taxonomy_term/nL39PHzAu3uXzCPe2BBSlw4VVVuZsLVS6RtbkaMLdS8?term=' + author_data.text + '&_type=query&q=' + author_data.text, function(data) {
+              if (data['results'].length > 0 && data['results'][0]['text'] === author_data.text) {
+                // Match found, use existing term ID for option
+                author_data.id = data['results'][0]['id'];
+              }
+              // Append option and select it
+              var option = new Option(author_data.text, author_data.id, true, true);
+              $('#edit-field-authors').append(option).trigger('change');
+            });
           });
 
-          // Field: Copyright
-          var copyright_data = {
-            id: '$ID:' + data['records'][0]['imageRights']['copyright'],
-            text: data['records'][0]['imageRights']['copyright']
-          };
-          // This URL should probably be resolved with regxp from #edit-field-copyrights...
-          $.getJSON('/select2_autocomplete/taxonomy_term/default:taxonomy_term/HSgHktR7ejpMhZd1Y-atx9Axw7SuNtwq0FT_sAh8smU?term=' + copyright_data.text + '&_type=query&q=' + copyright_data.text, function(data) {
-            if (data['results'][0]['text'] === copyright_data.text) {
-              // Match found, use existing term ID
-              copyright_data.id = data['results'][0]['id'];
-            }
-            // Append option and select it
-            var option = new Option(copyright_data.text, copyright_data.id, true, true);
-            $('#edit-field-copyrights').append(option).trigger('change');
+          // Field: Copyrights
+          var copyrights_data = data['records'][0]['imageRights'];
+
+          $(copyrights_data).each((index, element) => {
+            var copyright_data = {
+              id: '$ID:' + element['copyright'],
+              text: element['copyright']
+            };
+            // This URL should probably be resolved with regxp from #edit-field-copyrights...
+            $.getJSON('/select2_autocomplete/taxonomy_term/default:taxonomy_term/HSgHktR7ejpMhZd1Y-atx9Axw7SuNtwq0FT_sAh8smU?term=' + copyright_data.text + '&_type=query&q=' + copyright_data.text, function(data) {
+              if (data['results'][0]['text'] === copyright_data.text) {
+                // Match found, use existing term ID
+                copyright_data.id = data['results'][0]['id'];
+              }
+              // Append option and select it
+              var option = new Option(copyright_data.text, copyright_data.id, true, true);
+              $('#edit-field-copyrights').append(option).trigger('change');
+            });
           });
 
           // Field: Building
