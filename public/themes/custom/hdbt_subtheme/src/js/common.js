ede02b54a06a9bf8623eeebd073618ba632d9123
samijuv <sami.juvonen@citrus.fi>
2021-10-22 09:51:34 +0300

    HIS-221 Add 'Back to search results'-link if the user is coming from the search page

--- public/themes/custom/hdbt_subtheme/src/js/common.js
+++ public/themes/custom/hdbt_subtheme/src/js/common.js
@@ -1,9 +1,23 @@
 // eslint-disable-next-line no-unused-vars
-((Drupal, drupalSettings) => {
+(($, Drupal, drupalSettings) => {
   Drupal.behaviors.themeCommon = {
-    attach: function attach() {
-      
+    attach: function (context, settings) {
+
+      // Show 'Back to search results'-button if the user
+      // is coming from the search page
+      let previousUrl = document.referrer;
+      if (previousUrl && 
+        (previousUrl.includes('/haku') ||
+        previousUrl.includes('/search'))
+      ) {
+        $linkContainer = $('#back-to-search-results', context);
+
+        if ($linkContainer.length) {
+          $('a', $linkContainer).attr('href', previousUrl);
+          $linkContainer.show();
+        }
+      }
     },
   };
   // eslint-disable-next-line no-undef
-})(Drupal, drupalSettings);
+})(jQuery, Drupal, drupalSettings);
